061dc3cb45700604624331955bd0e4c1974e4c65
Kentaro Hayashi <kenhys@gmail.com>
2025-09-28 18:38:05 +0900

    test: use describe to group existing test cases

--- spec/rubocop/cop/lint/plugin_log_scope_spec.rb
+++ spec/rubocop/cop/lint/plugin_log_scope_spec.rb
@@ -3,33 +3,64 @@
 RSpec.describe RuboCop::Cop::Lint::FluentdPluginLogScope, :config do
   let(:config) { RuboCop::Config.new }
 
-  it 'registers an offense when using `$log` info or more important one without block' do
-    %w[info warn error fatal].each do |keyword|
-      expect_offense(<<~RUBY, keyword: keyword)
-        $log.%{keyword}("something")
-        ^{keyword}^^^^^^^^^^^^^^^^^^ Lint/FluentdPluginLogScope: Use plugin scope `log` instead of global scope `$log`.
-      RUBY
+  describe 'alert using global `$log` usage' do
+    it 'registers an offense when using `$log` info or more important one without block' do
+      %w[info warn error fatal].each do |keyword|
+        expect_offense(<<~RUBY, keyword: keyword)
+          $log.%{keyword}("something")
+          ^{keyword}^^^^^^^^^^^^^^^^^^ Lint/FluentdPluginLogScope: Use plugin scope `log` instead of global scope `$log`.
+        RUBY
 
-      expected = "log.#{keyword} \"something\"\n"
-      expect_correction(expected)
+        expected = "log.#{keyword} \"something\"\n"
+        expect_correction(expected)
+      end
+    end
+
+    it 'registers an offense when using `$log` {...}' do
+      %w[trace debug info warn error fatal].each do |keyword|
+        expect_offense(<<~RUBY, keyword: keyword)
+          $log.%{keyword} { "something" }
+          ^{keyword}^^^^^^^^^^^^^^^^^^^^^ Lint/FluentdPluginLogScope: Use plugin scope `log` instead of global scope `$log`.
+        RUBY
+
+        expect_correction(<<~RUBY)
+          log.#{keyword} { "something" }
+        RUBY
+      end
+    end
+
+    it 'registers an offense when using `$log` trace or debug without block' do
+      %w[trace debug].each do |keyword|
+        expect_offense(<<~RUBY, keyword: keyword)
+          $log.%{keyword}("something")
+          ^{keyword}^^^^^^^^^^^^^^^^^^ Lint/FluentdPluginLogScope: Use plugin scope `log` instead of global scope `$log`.
+        RUBY
+
+        expected = "log.#{keyword} { \"something\" }\n"
+        expect_correction(expected)
+      end
     end
   end
 
-  it 'does not register an offense when using `log`' do
-    expect_no_offenses(<<~RUBY)
-      log.info("something")
-    RUBY
-  end
-
-  it 'registers an offense when using `$log` trace or debug without block' do
-    %w[trace debug].each do |keyword|
-      expect_offense(<<~RUBY, keyword: keyword)
-        $log.%{keyword}("something")
-        ^{keyword}^^^^^^^^^^^^^^^^^^ Lint/FluentdPluginLogScope: Use plugin scope `log` instead of global scope `$log`.
+  describe 'no alert using `log` usage' do
+    it 'does not register an offense when using `log`' do
+      expect_no_offenses(<<~RUBY)
+        log.info("something")
       RUBY
+    end
 
-      expected = "log.#{keyword} { \"something\" }\n"
-      expect_correction(expected)
+    it 'does not register an offense when using `log` {...}' do
+      expect_no_offenses(<<~RUBY)
+        log.info { "something" }
+      RUBY
+    end
+
+    it 'does not register an offense when using `log.info` "\#{keyword}"' do
+      %w[info warn error fatal].each do |keyword|
+        expect_no_offenses(<<~RUBY, keyword: keyword)
+          log.#{keyword} "something \#{keyword}"
+        RUBY
+      end
     end
   end
 
@@ -42,33 +73,6 @@ RSpec.describe RuboCop::Cop::Lint::FluentdPluginLogScope, :config do
       RUBY
       expect_correction(<<~RUBY)
         log.#{keyword} { "something \#{keyword}" }
-      RUBY
-    end
-  end
-
-  it 'registers an offense when using `$log` {...}' do
-    %w[trace debug info warn error fatal].each do |keyword|
-      expect_offense(<<~RUBY, keyword: keyword)
-        $log.%{keyword} { "something" }
-        ^{keyword}^^^^^^^^^^^^^^^^^^^^^ Lint/FluentdPluginLogScope: Use plugin scope `log` instead of global scope `$log`.
-      RUBY
-
-      expect_correction(<<~RUBY)
-        log.#{keyword} { "something" }
-      RUBY
-    end
-  end
-
-  it 'does not register an offense when using `log` {...}' do
-    expect_no_offenses(<<~RUBY)
-      log.info { "something" }
-    RUBY
-  end
-
-  it 'does not register an offense when using `log.info` "\#{keyword}"' do
-    %w[info warn error fatal].each do |keyword|
-      expect_no_offenses(<<~RUBY, keyword: keyword)
-        log.#{keyword} "something \#{keyword}"
       RUBY
     end
   end
